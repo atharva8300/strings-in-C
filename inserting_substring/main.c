364fea1f3bb12966191c57af19ca673e3404d676
buddy8300 <atharva8300@gmail.com>
2020-07-15 16:15:58 +0530

    finally inserted the substring-_-

--- inserting_substring/main.c
+++ inserting_substring/main.c
@@ -1,42 +1,54 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
 #include <string.h>
-void insert_substring(char sub[],char mainstr[],int pos);
+void insert_substring(char sub[],char mainstr[],char finalstr[],int pos);
 int main()
 {
-    char ms[50],ss[20];
+    char ms[20],ss[20],fs[50];
     int findex ;
-    printf("enter main string [less than 30 chars]:");
+    printf("enter main string:");
     gets(ms);
     printf("\nenter substring to be inserted : ");
     gets(ss);
     printf("\nenter position at which substring has to be inserted : ");
     scanf("%d",&findex);
-    insert_substring(ss,ms,findex);
-    puts(ms);
+    insert_substring(ss,ms,fs,findex);
+    printf("\n");
+    puts(fs);
     getch();
     return 0;
 }
-void insert_substring(char sub[],char mainstr[],int pos)
+void insert_substring(char sub[],char mainstr[],char finalstr[],int pos)
 {
-    int i,j=0,n;
-    n=strlen(mainstr);
-    i=n;
-    printf("inside func %d%d",i,pos);
-    while(i>=pos)
+    int i=0,j=0,k=0;
+    int lenmain,lensub,lenfinal;
+    lenmain = strlen(mainstr);
+    lensub = strlen(sub);
+    lenfinal = lensub+lenmain;
+    printf("%d",lenfinal);
+    while(i<=lenfinal)
     {
-        printf("inside while1");
-        mainstr[i+1]=mainstr[i];
-        i--;
-    }
+        if(i==pos)
+        {
+            printf("in if");
+            while(j<lensub)
+            {
+                printf("in while");
+                finalstr[i]=sub[j];
+                j++;
+                i++;
+            }
+        }
+        else
+        {
+            printf("in else");
+            finalstr[i]=mainstr[k];
+            k++;
+            i++;
+        }
 
-    i = pos;
-    while(sub[j] != '\0' )
-    {
-        printf("in while2 ");
-        mainstr[i]=sub[j];
-        i++;
-        j++;
     }
+    finalstr[i]='\0';
 
 }
